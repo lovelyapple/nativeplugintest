1d80da4e5a39a09b8644144f7f4a061af39abd98
lovelyapple <lovelyappleoffice@gmail.com>
2020-08-16 23:56:21 +0900

    test build

---
+++ Caravanandroidhelper/ScreenHelper/src/main/java/com/example/screenhelper/NativeCalculator.java
@@ -0,0 +1,38 @@
+package com.example.screenhelper;
+
+public class NativeCalculator {
+    /**
+     * 前回の計算結果
+     */
+    private float _lastResult = 0;
+
+    /**
+     * コンストラクタ
+     */
+    public NativeCalculator()
+    {
+        _lastResult = 0;
+    }
+
+    /**
+     * 足す
+     * @param num 足す値
+     * @return 計算結果
+     */
+    public float Add(int num)
+    {
+        _lastResult += num;
+        return _lastResult;
+    }
+
+    /**
+     * 引く
+     * @param num 引く値
+     * @return 計算結果
+     */
+    public float Subtract(int num)
+    {
+        _lastResult -= num;
+        return _lastResult;
+    }
+}
---
+++ TestAndroidNativePlugin/Assets/Scripts/AndroidHelper.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class AndroidHelper : MonoBehaviour
+{
+    /// <summary>
+    /// プラグイン側の計算クラスのインスタンス
+    /// </summary>
+    private AndroidJavaObject _calculator;
+
+    /// <summary>
+    /// プラグインのパッケージ名
+    /// </summary>
+    private const string PLUGIN_PACKAGE_NAME = "com.example.screenhelper";
+
+    /// <summary>
+    /// Javaの計算クラス名
+    /// </summary>
+    private const string JAVA_CLASS_NAME = "NativeCalculator";  // 要変更
+    /// <summary>
+    /// Start
+    /// </summary>
+    void Start()
+    {
+        // インスタンス生成
+        _calculator = new AndroidJavaObject("com.example.screenhelper.NativeCalculator");
+
+        Debug.Log("_calculator " + _calculator != null);
+    }
+    /// <summary>
+    /// OnDestroy
+    /// </summary>
+    private void OnDestroy()
+    {
+        // 解放
+        _calculator?.Dispose();
+        _calculator = null;
+    }
+
+
+
+    private const string ADD_METHOD = "Add";
+    private const string SUBTRACT_METHOD = "Subtract";
+    public void add(int num)
+    {
+        var result = _calculator.Call<float>(ADD_METHOD, num);  // 計算結果
+
+        Debug.Log("result " + result);
+    }
+    public void subtract(int num)
+    {
+        var result = _calculator.Call<float>(SUBTRACT_METHOD, num);  // 計算結果
+        Debug.Log("result " + result);
+    }
+}
+
+[CustomEditor(typeof(AndroidHelper))]
+public class AndroidHelperEditor : Editor
+{
+    int numberToDo;
+    public override void OnInspectorGUI()
+    {
+        AndroidHelper helper = target as AndroidHelper;
+        numberToDo = EditorGUILayout.IntField("number", +numberToDo);
+
+        if (GUILayout.Button("add"))
+        {
+            helper.add(numberToDo);
+        }
+
+        if (GUILayout.Button("sub"))
+        {
+            helper.subtract(numberToDo);
+        }
+    }
+}
